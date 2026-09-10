6739590fb596ea36d11f9740c2b5089bf4ccbcec
prat <prat070905@gmail.com>
2026-09-10 21:57:37 +0530

    chore: implement fft and helper complex class

--- backend/src/main/java/com/pratham/App.java
+++ backend/src/main/java/com/pratham/App.java
@@ -2,28 +2,24 @@ package com.pratham;
 
 import com.pratham.audioutils.AudioExporter;
 import com.pratham.audioutils.MicrophoneRecorder;
+import com.pratham.shazam.Complex;
+import com.pratham.shazam.FFT;
 
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main( String[] args ) throws Exception {
 
+        //Record audio
         MicrophoneRecorder recorder = new MicrophoneRecorder();
         short[] audioData = recorder.record(5);
-//        System.out.println("Samples: " + audioData.length);
-//        for(int i=0; i<10; i++){
-//            System.out.println(audioData[i]);
-//        }
 
         AudioExporter.save(audioData, "src/main/assets/audio.csv");
-        // Automatically run Python
+        // Automatically run Python file to generate plot
         Process process = new ProcessBuilder(
                 ".venv/bin/python",
                 "src/main/assets/python/plot_audio.py"
         ).inheritIO().start();
         process.waitFor();
         System.out.println("Created audio visual.");
+
     }
 }
---
+++ backend/src/main/java/com/pratham/shazam/Complex.java
@@ -0,0 +1,47 @@
+package com.pratham.shazam;
+
+public class Complex {
+    private final double real;
+    private final double imaginary;
+
+    public Complex(double real, double imaginary){
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    @Override
+    public String toString() {
+        return (real + " + "  + imaginary + "i");
+    }
+
+    //add
+    public Complex add(Complex other){
+        return new Complex(this.real + other.real, this.imaginary + other.imaginary);
+    }
+
+    //subtract
+    public Complex subtract(Complex other){
+        return new Complex(this.real - other.real, this.imaginary - other.imaginary);
+    }
+
+    //multiply
+    public Complex multiply(Complex other){
+        double realPart = this.real * other.real - this.imaginary * other.imaginary;
+        double imaginaryPart = this.real * other.imaginary + this.imaginary * other.real;
+
+        return new Complex(realPart, imaginaryPart);
+    }
+
+    //magnitude
+    public double magnitude(){
+        return Math.sqrt(real*real + imaginary*imaginary);
+    }
+}
